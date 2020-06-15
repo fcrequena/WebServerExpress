e8c5bcca1c16138c5fe75626216d165bc115680e
Francisco <francisco.requenacaal@gmail.com>
2020-06-14 22:16:37 -0600

    UPD - Se grega HBS

--- server.js
+++ server.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const app = express();
 
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', (req, res) => {
-//     //res.send('Hello World')
-//     let salida = {
-//         nombre: 'francisco',
-//         edad: 27,
-//         url: req.url
-//     }
-//     res.send(salida);
-// })
+//express HBS ENGINE
+hbs.registerPartials(__dirname + '/views/parcials');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'francisCO'
+    });
+
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+//app.get('/', (req, res) => {
+//res.send('Hello World')
+// let salida = {
+//     nombre: 'francisco',
+//     edad: 27,
+//     url: req.url
+// }
+//res.send(salida);
+//})
 
 // app.get('/data', (req, res) => {
 //     res.send('Hola data');
 // })
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puesto 3000')
+
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puesto ${port}`)
 });
